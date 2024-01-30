49e9e85dffacc101ef1228a6d796685d71cceebd
yolandahe <he.yang7@northeastern.edu>
2024-01-30 13:30:51 -0800

    week 3

--- App.js
+++ App.js
@@ -1,30 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, SafeAreaView} from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
+import React, { useState } from 'react';
 
 export default function App() {
-  const appName = "my awesome app"; 
+  const appName = "my awesome app";
+  const [text, setText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  function receiveInput(data){
+    console.log("receive input", data);
+    setText(data);
+    setIsModalVisible(false);
+  }
+  function dismissModal(){
+    setIsModalVisible(false);
+  }
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Header name = {appName} version = {2}/>
-      <Input />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topView}>
+        <StatusBar style="auto" />
+        <Header name = {appName} version = {2}/>
+        <Button title="Add a goal" onPress={() => setIsModalVisible(true)}/>
+        <Input 
+          inputHandler={receiveInput} 
+          modalVisible={isModalVisible} 
+          dismissModal={dismissModal}/>
+      </View>
+      <View style={styles.bottomView}>
+        <Text>{text}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "white",
+    //alignItems: 'center',
     justifyContent: 'center',
   },
 
-  input:{
-    borderBottomWidth:2,
-    borderBottomColor: "purple"
-  }
+  topView:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+
+  bottomView:{
+    flex:4,
+    backgroundColor: "lightpink",
+    alignItems:"center",
+  },
+
+  text:{
+    textAlign: "center",
+  },
 });
--- components/Input.js
+++ components/Input.js
@@ -1,35 +1,71 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, Modal, Image } from 'react-native'
 import React, { useState } from 'react'
 
-export default function Input() {
+export default function Input({inputHandler, modalVisible, dismissModal}) {
   const [text, setText] = useState("");
 
-  //callback function
-  function changeTextHandler(changedText){
-    console.log("user is typing", changedText);
-    setText(changedText);
-    }
+  function confirmHandler(){
+    inputHandler(text);
+    setText("");
+  }
 
   return (
-    <TextInput 
-    placeholder = "Type something"
-    style = {styles.input} 
-    value = {text} 
-    onChangeText = {changeTextHandler}
-    />
+    <Modal visible = {modalVisible}>
+      <View style = {styles.container}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+          }}
+          style={styles.image}/>
+        <Image 
+          source = {require("../assets/goal.png")}
+          style={styles.image} />
+        <TextInput 
+          placeholder = "Type something"
+          style = {styles.input} 
+          value = {text} 
+          onChangeText = {(text) => setText(text)}
+        />
+        <View style = {styles.buttonsContainer}>
+          <View style={styles.buttonView}>
+            <Button title="Cancel" onPress={dismissModal} />
+            </View>
+          <View style={styles.buttonView}>
+            <Button title="Confirm" onPress={confirmHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+
   )
 }
 
 const styles = StyleSheet.create({
+    buttonView:{
+      width: "30%",
+      margin:5,
+    },
+
+    buttonsContainer:{
+      flexDirection: "row"
+    },
+
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#ccc',
       alignItems: 'center',
       justifyContent: 'center',
     },
   
     input:{
       borderBottomWidth:2,
-      borderBottomColor: "purple"
-    }
+      borderBottomColor: "purple",
+      width: "50%",
+    },
+
+    image: {
+      width: 100,
+      height: 100
+    },
+
   });
