0b6e6099e365c3a0800fa405820fb292d4250f35
yolandahe <he.yang7@northeastern.edu>
2024-04-01 21:28:18 -0700

    added location manager and show user's location on a static map

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,21 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import * as Location from 'expo-location';
-
-const locateUserHandler = async () => {
-    try {
-        const location = await Location.getCurrentPositionAsync();
-    } catch (err) {   
-        console.error(err);
-    }
-}
+import { mapsApiKey } from "@env";
 
 export default function LocationManager() {
+  const [location, setLocation] = useState(null);
+  const [status, requestPermission] = Location.useForegroundPermissions();
+
+  async function verifyPermission() {
+      if (status.granted) {
+          return true;
+      }
+      if (status !== 'granted') {
+          try {
+              const permissionResponse = await requestPermission();
+              return permissionResponse.granted;
+          } catch (err) {
+              console.error(err);
+          }
+      }
+  }
+
+  async function locateUserHandler(){
+      try {
+          const hasPermission = await verifyPermission();
+          if (!hasPermission) {
+              Alert.alert("You need to give permission to access location.")
+              return;
+          }
+          const receivedLocation = await Location.getCurrentPositionAsync();
+          setLocation({
+              latitude: receivedLocation.coords.latitude,
+              longitude: receivedLocation.coords.longitude,
+          })
+      } catch (err) {   
+          console.error(err);
+      }
+  }
+
   return (
     <View>
-      <Text>LocationManager</Text>
+      <Button title="Locate me" onPress={locateUserHandler} />
+      {location && (
+        <Image 
+        style={styles.image}
+        source={{
+          uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&markers=color:red%7Clabel:A%7C${location.latitude},${location.longitude}&key=${mapsApiKey}`,
+        }}/>
+          
+      )}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image:{
+    width: Dimensions.get('window').width,
+    height: 200,
+  }
+})
